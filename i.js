78fe4c7820b1bfa8659d8279a39275949708ee5a
Sanmu3 <mujahidmuslim0@gmail.com>
2020-09-18 21:31:30 +0700

    tugas no 1

---
+++ i.js
@@ -0,0 +1,7 @@
+const santriIkhwan = ['sidik', 'rahmad', 'sofyan', 'aldi','kardi']
+
+const [ Lampung, Prabumulih, ...Lain ] = santriIkhwan;
+
+console.log(Lampung);
+console.log(Prabumulih);
+console.log(Lain);
